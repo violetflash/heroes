4b6b959b300d3db5019521d9e7ee1f1d0964a3bf
Alex Grobovsky <ktotaam@mail.ru>
2021-05-05 19:57:07 +0500

    save

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*
 Задания на КОНКУРС ЛУЧШИХ РАБОТ
 1) Загрузить JSON файл
@@ -16,75 +14,121 @@
     В случае идеального кода у претендентов - будем смотреть на стили.
 */
 
-function animate({ timing, draw, duration }) {
-    const start = performance.now();
-    requestAnimationFrame(function animate(time) {
-        // timeFraction изменяется от 0 до 1
-        let timeFraction = (time - start) / duration;
-        if (timeFraction > 1) {
-            timeFraction = 1;
-        }
-        // вычисление текущего состояния анимации
-        const progress = timing(timeFraction);
-        draw(progress); // отрисовать её
-        if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-        }
-    });
-}
-
 const headerImg = document.querySelector('.header__img');
 
-const headerImgArrival = () => {
-    // animate({
-    //     duration: 2500,
-    //     timing(timeFraction) {
-    //         return 1 - Math.sin(Math.acos(timeFraction));
-    //     },
-    //     draw(progress) {
-    //         console.log(progress);
-    //         // console.log(headerImg.offsetTop);
-    //         // console.log(headerImg.offsetHeight);
-    //         if (headerImg.offsetTop < 0) {
-    //             headerImg.style.top = -headerImg.offsetHeight + progress * 1000 + 'px';
-    //         }
-    //     }
-    // });
-    let counter = 0;
-    const moveImg = () => {
-        if (headerImg.offsetTop < 0) {
-            headerImg.style.top = 10 + counter + 'px';
-        }
-        counter += 10;
-    };
-    const headerInterval = setInterval(moveImg, 1000);
-    headerImg.style.position = 'block';
-};
+// const headerImgArrival = () => {
+//     const moveImg = (img) => {
+//         const imgTop = img.getBoundingClientRect().top;
+//         console.log(imgTop);
+//         if (imgTop < 0) {
+//             img.style.top = imgTop + 1 + 'px';
+//
+//         } else {
+//             clearInterval(headerInterval);
+//         }
+//     };
+//     const headerInterval = setInterval(moveImg, 0.1, headerImg);
+//     // headerImg.style.position = 'block';
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
-    headerImgArrival();
+    'use strict';
+
+    class HeroesCards {
+        constructor({root, db, filterBlock, cardsBlock}) {
+            this.root = document.querySelector(root);
+            this.db = db;
+            this.movies = new Set();
+            this.filterBlock = filterBlock;
+            this.cardsBlock = cardsBlock;
+        }
+
+        async getMoviesData() {
+            const response = await fetch(this.db);
+            // console.log(result);
+            const result = await response.json();
+            console.log(result);
+            const moviesSet = new Set();
+            const actorsSet = new Set();
+
+            result.forEach(element => {
+                if (element.movies) {
+                    element.movies.forEach(movie => moviesSet.add(movie));
+                }
+                if (element.actors) {
+                    element.movies.forEach(movie => moviesSet.add(movie));
+                }
+            });
+            console.log(this.movies);
+            this.createFilters();
+        }
+
+        createBase() {
+            this.root.innerHTML = `
+                <h1 class="title">Find your Hero!</h1>
+                <div class="heroes">
+                    <div class="filter">
+                        
+                    </div>
+                    <div class="cards"></div>
+                </div>
+            `;
+        }
+
+        createFilters() {
+            //iterate movies Set to create a link for each movie
+            const filter = document.querySelector('.filter');
+
+            //create movies select
+            const moviesSelect = document.createElement('select');
+            const moviesLabel = document.createElement('label');
+            moviesLabel.for = 'movie-option';
+            moviesLabel.textContent = 'filter by film:';
+            this.movies.forEach(elem => {
+                const option = document.createElement('option');
+                option.textContent = elem;
+                option.id = 'movie-option';
+                moviesSelect.append(option);
+            });
+            filter.append(moviesLabel);
+            filter.append(moviesSelect);
 
 
-});
+        }
+
+        addEventListeners() {
+            const filter = document.querySelector('.filter');
+            filter.addEventListener('mouseover', () => {
+                filter.classList.add('js-active');
+            });
+
+            filter.addEventListener('mouseout', () => {
+                filter.classList.remove('js-active');
+            });
+
+        }
 
 
 
-const firstReq = new XMLHttpRequest();
-firstReq.overrideMimeType("application/json");
-
-firstReq.addEventListener('load', function() {
-    console.log('FIRST REQUEST WORKED!');
-    const data = JSON.parse(this.responseText);
-    for (const hero of data) {
-        console.log(hero.name);
+        init() {
+            this.createBase();
+            this.getMoviesData();
+            console.log(this.movies);
+            this.addEventListeners();
+        }
     }
-    console.log(data);
+
+    //https://stackoverflow.com/questions/2722159/how-to-filter-object-array-based-on-attributes
+    //iterate array async
+
+
+    //create instance of heroes class
+    const cards = new HeroesCards({
+        root: '.content',
+        db: './base.json',
+    });
+
+    cards.init();
+
 });
 
-firstReq.addEventListener('error', () => {
-    console.log('ERROR!');
-});
-
-firstReq.open('GET', './base.json');
-firstReq.send();
-console.log('Request Sent!');
